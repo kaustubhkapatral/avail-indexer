d31fdca275341d5347a3ad4911b7d2735b8b6332
kaustubhkapatral <kaustubh.kapatral@gmail.com>
2023-11-15 11:50:15 +0530

    fixes

--- autorestart.sh
+++ autorestart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Checking to see if all the containers are operational. If any container is down then docker compose pulls it down and restarts it 
 CONTAINERS=$(sudo docker ps --format '{{.Names}}' | wc -l)
 
 if [ $CONTAINERS -lt 3 ]; then
     echo "Restarting Docker containers..."
     cd ~/avail-indexer
-    sudo docker-compose down
-    nohup sudo docker-compose up  </dev/null &>/dev/null &
+    sudo docker compose down
+    sudo docker compose up -d
     echo "Restarted"
 fi
 
